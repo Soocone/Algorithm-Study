b5f19ceed55320da24af264b6af6ef9dd7ed7772
Soocone <wltn3185@naver.com>
2022-01-02 00:02:53 +0900

    0101 study

--- 1-2-7InsertionSort.py
+++ 1-2-7InsertionSort.py
@@ -7,7 +7,7 @@
             세번째 데이터와 그 앞 데이터들의 크기를 비교하여 삽입될 인덱스를 찾고 
             해당위치의 데이터들은 뒤로 보내는 것을 반복하여 숫자를 정렬한다.
 '''
-
+# 내 답안(못풀었음)
 def insertion(lists):
     for i in range(1, 7):
         for chr in lists:
@@ -21,9 +21,26 @@ def insertion(lists):
 print(insertion([4,6,2,8,1,9,3]))
 
 
+#윤아언니 답
+def Insertion(lists):
+    temp = 0
+    for i in range(1, len(lists)):
+        myIdx = i #내가 있어야할 인덱스
+        for j in range(i-1, -1, -1):
+            if lists[i]<lists[j]:
+                myIdx = j
+                
+        if i != myIdx: #내위치가 바뀌어야할 경우
+            temp = lists[i]
+            for k in range(i, myIdx-1, -1):
+                lists[k] = lists[k-1]
+            lists[myIdx] = temp
+            
+    return lists
 
 
-# 사이트 답
+
+# 사이트 어떤 사람 답안인데 좀 이상함
 def insertion_sorting(list):
     for i in range(1, len(list)):
         max_index = i
