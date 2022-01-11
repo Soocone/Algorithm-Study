5fb39de44b0ad794f87c24c95b2f217c14193427
Soocone <wltn3185@naver.com>
2022-01-11 21:03:49 +0900

    0111 study

--- 2-2-6hanoi.py
+++ 2-2-6hanoi.py
@@ -15,9 +15,24 @@ def move_disk(disk_num, start_peg, end_peg):
 
 def hanoi(num_disks, start_peg, end_peg):
     # 코드를 입력하세요.
-
+    if num_disks == 0:
+        return
+    else:
+        other_peg = 6 - start_peg - end_peg
+        
+        # 1. 가장 큰 원판을 제외하고 나머지 원판들을 start_peg에서 other_peg로 이동
+        hanoi(num_disks - 1, start_peg, other_peg)
+        
+        # 2. 가장 큰 원판을 start_peg에서 end_peg로 이동
+        move_disk(num_disks, start_peg, end_peg)
+        
+        # 3. 나머지 원판들을 other_peg에서 end_peg로 이동
+        hanoi(num_disks - 1, other_peg, end_peg)
+        
+        
 # 테스트 코드 (포함하여 제출해주세요)
 hanoi(3, 1, 3)
+
 
 
 '''
